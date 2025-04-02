1c712a75bceb45431962f5790f24a18a7e721538
comerciial88 <comerciial88@gmail.com>
2025-04-02 01:21:47 -0400

    Update carrito.js

--- carrito.js
+++ carrito.js
@@ -94,12 +94,12 @@ function copiarListaCarrito() {
     const total = document.getElementById("total").innerText;
 
     // Crear el texto para copiar
-    const mensaje = `Hola quiero lo siguiente:\n${productos}\n\nTotal: $${total}`;
+    const mensaje = `Hola Quisiera:\n${productos}\n\nTotal: $${total}`;
 
     // Copiar al portapapeles
     navigator.clipboard.writeText(mensaje).then(() => {
         // Mostrar un mensaje en pantalla
-        alert("Lista copiada, dele pegar a cualquier chat, gracias por venir");
+        alert("Su Lista a sido Copiada");
     }).catch(err => {
         // En caso de error
         alert("Hubo un problema al copiar la lista");
